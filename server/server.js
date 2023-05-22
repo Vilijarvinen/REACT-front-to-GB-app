92c3db35b4709e96eed5004c1c4ce4c84fb2201f
Vilijarvinen <vili.ja@pp.inet.fi>
2023-05-22 22:52:14 +0300

    asd

--- server/server.js
+++ server/server.js
@@ -1,18 +1,28 @@
 require('dotenv').config();
 const express = require("express");
+const cors = require("cors");
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 9000;
 const app = express();
 const bodyParser = require('body-parser');
 const schemas = require("./sche");
+// const path = require('path')
 
-app.use(bodyParser.json({extended: false}));
-app.use(bodyParser.urlencoded({extended: false}));
+// const buildPath = path.join(__dirname, "..", "/client/build")
+
+app.use(bodyParser.json({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.static(buildPath))
+// app.use(express.json())
 
 mongoose.connect(process.env.URIKEY).then(() => console.log('db connected'))
 
+// app.use((req, res) => {
+//     res.render(path.join(__dirname, "..", "/client/build", "index.html"));
+// });
+
 app.get("/api", (req, res) => {
-    schemas.find().then(function(a){
+    schemas.find().then(function (a) {
         console.log(JSON.stringify(a))
         return res.send(JSON.stringify(a));
     })
@@ -21,9 +31,9 @@ app.get("/api", (req, res) => {
 app.post("/api", (req, res) => {
     console.log(req.body);
     try {
-        schemas.deleteMany({ id:{"$gte":1} }).then(function(){
+        schemas.deleteMany({ id: { "$gte": 1 } }).then(function () {
             schemas.create(req.body);
-        }).catch(function(error){
+        }).catch(function (error) {
             console.log(error);
         });
     }
